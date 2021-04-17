648c4f346479ba09ab35e997fdb03c8d1d575d04
Abdullahi Salihu <abdullahisalihu318@gmail.com>
2021-04-17 03:45:10 -0700

    Add lesson1.py file

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+name = input("Hi! What’s your name? ")
+
+age = input("How old are you ")
+
+print("Nice to meet you " + name + "!")
+
+print("So, you are already " + str(age) + " years old, " + name + "!")
